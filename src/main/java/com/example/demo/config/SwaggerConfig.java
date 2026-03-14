ed06cd1a25e3b6fd1d36d2be54bba6ce9d2798f2
PradhumnaG <pradhumnagaudel0@gmail.com>
2026-03-14 22:21:17 +0530

    added swagger dependency

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,34 +1,17 @@
-package com.example.demo.config;
+ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class SwaggerConfig {
+ @Configuration
+ public class SwaggerConfigNew {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("JWT Spring Boot API")
-                        .description("API documentation for JWT Spring Boot Application")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("API Support")
-                                .url("http://example.com")))
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("Bearer Authentication",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                                        .description("JWT auth description")));
-    }
-}
-
+     public OpenAPI customOpenAPI() {
+         return new OpenAPI()
+                 .info(new Info()
+                         .title("Hello Doctor API")
+                         .version("1.0")
+                         .description("API documentation for the Hello Doctor application"));
+     }
+ }
